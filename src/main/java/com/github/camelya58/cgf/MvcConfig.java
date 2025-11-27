1b7886bcd5209961887717e23a1f17d915ef2c74
jose torres <josetg@mail.com>
2025-11-27 14:34:50 -0500

    Spring Boot 3 + Jetty + JSP

--- src/main/java/com/github/camelya58/cgf/MvcConfig.java
+++ src/main/java/com/github/camelya58/cgf/MvcConfig.java
@@ -1,21 +1,17 @@
 package com.github.camelya58.cgf;
 
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
-
+@Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
-
+@Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setViewClass(JstlView.class); // Use JstlView if JSTL is in classpath
